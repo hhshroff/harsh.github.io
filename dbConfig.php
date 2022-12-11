0b30823fbb4d08b95330ff09baedf36da5fbfbff
Rushil Choksi <55545647+rushilchoksi@users.noreply.github.com>
2022-12-11 17:36:23 +0530

    Added PHP functionality

---
+++ dbConfig.php
@@ -0,0 +1,22 @@
+<?php
+function getFooter(){
+    echo '<div class="footer-bottom">
+    <div class="inner-content text-align-center">
+      <div class="link-footer-wrapper">
+        <div class="text-size-small text-color-grey">© Jacob — Powered by</div>
+        <a href="https://webflow.com/" target="_blank" class="link-footer w-inline-block">
+          <img src="https://assets.website-files.com/62b38327b8e2220e110a72b0/62c76c28f4518a06e3eb518c_webflow-logo-white.svg" loading="lazy" alt="Webflow Logo - Jacob Webflow Template" class="webflow-logo" />
+        </a>
+      </div>
+    </div>
+    <div class="inner-content text-align-center">
+      <div class="link-footer-wrapper">
+        <div class="text-size-small text-color-grey">Made by</div>
+        <a href="https://elison.webflow.io/" target="_blank" class="link-footer w-inline-block">
+          <div>Elison Santos</div>
+        </a>
+      </div>
+    </div>
+  </div>';
+}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require_once('dbConfig.php');
+?>
 <!DOCTYPE html>
 <!-- This site was created in Webflow. https://www.webflow.com -->
 <!-- Last Published: Fri Nov 25 2022 18:04:42 GMT+0000 (Coordinated Universal Time) -->
@@ -706,24 +709,7 @@
                   </div>
                 </div>
               </div>
-              <div class="footer-bottom">
-                <div class="inner-content text-align-center">
-                  <div class="link-footer-wrapper">
-                    <div class="text-size-small text-color-grey">© Jacob — Powered by</div>
-                    <a href="https://webflow.com/" target="_blank" class="link-footer w-inline-block">
-                      <img src="https://assets.website-files.com/62b38327b8e2220e110a72b0/62c76c28f4518a06e3eb518c_webflow-logo-white.svg" loading="lazy" alt="Webflow Logo - Jacob Webflow Template" class="webflow-logo" />
-                    </a>
-                  </div>
-                </div>
-                <div class="inner-content text-align-center">
-                  <div class="link-footer-wrapper">
-                    <div class="text-size-small text-color-grey">Made by</div>
-                    <a href="https://elison.webflow.io/" target="_blank" class="link-footer w-inline-block">
-                      <div>Elison Santos</div>
-                    </a>
-                  </div>
-                </div>
-              </div>
+              <?php getFooter(); ?>
             </div>
           </div>
         </div>
