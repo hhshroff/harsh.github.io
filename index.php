9f6fd7c326ece72fd7c82a5178b114d2fcb09604
Harsh Shroff <shroff.hh@gmail.com>
2023-03-14 16:57:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ require_once('dbConfig.php');
                 <div class="header-content-wrapper">
                   <div class="overflow-hidden">
                     <div class="horizontal-flex margin-right margin-small">
-                      <h1 class="heading-huge text-color-dim-grey">i&#x27;m Harsh</h1>
+                      <h1 class="heading-huge text-color-dim-grey">i&#x27;m Harsh H</h1>
                       <div style="opacity:0" class="copyright text-color-dim-grey">©</div>
                     </div>
                   </div>
